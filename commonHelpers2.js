c3f14530fa7b6d4fb094d477b7418712057521c9
koleso8 <168295877+koleso8@users.noreply.github.com>
2024-07-10 15:43:32 +0000

    Deploying to gh-pages from @ koleso8/goit-js-hw-09@1909b683a265160cb941a271ab9accf4448d0345 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const a={formEl:document.querySelector(".feedback-form"),inputEl:document.querySelector("[name = email]")},t={email:"",message:""};a.formEl.addEventListener("input",e=>{e.preventDefault(),Object.keys(t)[0]===e.target.name?t.email=e.target.value:t.message=e.target.value,console.log(t)});
+//# sourceMappingURL=commonHelpers2.js.map
