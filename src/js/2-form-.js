13e2a440b9125c3cb9405502c25939a4ab18abfd
koleso8 <dev.koleso@gmail.com>
2024-07-11 08:45:18 +0300

    Update 2-form-.js

--- src/js/2-form-.js
+++ src/js/2-form-.js
@@ -2,7 +2,7 @@ const refs = {
   formEl: document.querySelector('.feedback-form'),
   inputEl: document.querySelector('[name = email]'),
 };
-const formData = { email: ``, message: '' };
+let formData = { email: '', message: '' };
 
 refs.formEl.addEventListener('input', e => {
   e.preventDefault();
@@ -12,5 +12,25 @@ refs.formEl.addEventListener('input', e => {
     formData.message = e.target.value;
   }
 
-  console.log(formData);
+  saveToLs('feedback-form-state', formData);
+
+  function saveToLs(key, value) {
+    const json = JSON.stringify(value);
+    localStorage.setItem(key, json);
+    console.log(json);
+  }
 });
+const loadStorage = loadFromLs('feedback-form-state');
+
+function loadFromLs(key) {
+  const json = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(json);
+    return data;
+  } catch {
+    return json;
+  }
+}
+refs.formEl.elements.email.value = loadStorage.email;
+refs.formEl.elements.message.value = loadStorage.message;
+console.log(load);
